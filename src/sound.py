aa022c8b31c5d059664c96617a187d42d45dc77b
mingkhoi <mingkhoitran1234@gmail.com>
2021-12-10 01:34:56 +0700

    wav image mono color

--- src/sound.py
+++ src/sound.py
@@ -6,18 +6,27 @@ import partition
 
 this_dir = os.getcwd()
 
+wav_form_color = "0EAE59"
 
-def get_wav(parent_dir, full_file_dir: str):
+
+def get_wav(parent_dir, full_file_dir: str, width = 600, height = 120):
 
     out_file_name = "temp/" + str(uuid.uuid4()) + ".png"
 
     bash_cmd = f"""
         ffmpeg -i {full_file_dir} \
-            -filter_complex compand,showwavespic=s=640x120 \
+            -filter_complex \
+                "[0:a]aformat=channel_layouts=mono, \
+                compand=gain=-6, \
+                showwavespic=s={width}x{height}:colors=#{wav_form_color}[fg]; \
+                color=s={width}x{height}:color=#FFFFFF@0.0, \
+                drawgrid=width=iw/10:height=ih/5:color=#FFFFFF@0.0[bg]; \
+                [bg][fg]overlay=format=auto,drawbox=x=(iw-w)/2:y=(ih-h)/2:w=iw:h=1:color=#{wav_form_color}" \
             -frames:v 1 {out_file_name}
     """
     bash_cmd = bash_cmd.strip()
-    process = subprocess.Popen(bash_cmd.split(), bufsize=2048, stdout=subprocess.PIPE)
+
+    process = subprocess.Popen(bash_cmd, bufsize=2048, stdout=subprocess.PIPE, shell=True)
     process.wait()
 
     if process.returncode == 0:
