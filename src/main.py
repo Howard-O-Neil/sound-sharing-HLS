6efa1cce482eaf81a26b5e0e365f30881d0f4ccf
mingkhoi <mingkhoitran1234@gmail.com>
2021-12-10 23:58:45 +0700

    kill ffmpeg sub process

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template, request, send_from_directory
 import partition
 import sound
 
+import signal
 import json
 import os
+import subprocess
 
 from dotenv import load_dotenv
 
@@ -13,6 +15,9 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__)) # get parent of this folder
 load_dotenv(os.path.join(BASEDIR, "development.env"))
 
 CDN_DIR = os.getenv("CDN_DIR")
+STREAM_SERVER = os.getenv("STREAM_SERVER")
+
+stream_process = {}
 
 app = Flask(__name__)
 
@@ -24,6 +29,53 @@ def hello():
 def serve_static():
     file_id = request.args.get("file-id")
     return send_from_directory(CDN_DIR, file_id)
+
+@app.route("/start-stream", methods=["GET"])
+def start_sound_stream():
+    sound_id = request.args.get("sound-id")
+
+    if sound_id not in stream_process:
+        stream_process[sound_id] = {
+            1: None,
+            2: None,
+        }
+        stream_process[sound_id][1], random_str = sound.ffmpeg_stream(CDN_DIR, sound_id)
+        stream_process[sound_id][2] = sound_id.replace("/", random_str)
+
+        return json.dumps({
+            "hls-url": f"http://{STREAM_SERVER}/hls/{stream_process[sound_id][2]}.m3u8",
+            "dash-url":  f"http://{STREAM_SERVER}/dash/{stream_process[sound_id][2]}.mpd",
+            "rtmp-url": f"rtmp://{STREAM_SERVER}:1935/show/{stream_process[sound_id][2]}"
+        })
+    else:
+        print("===============================")
+        print(stream_process[sound_id][1].pid)
+        return json.dumps({
+                "error": 400,
+                "message": "stream already served",
+                "hls-url": f"http://{STREAM_SERVER}/hls/{stream_process[sound_id][2]}.m3u8",
+                "dash-url":  f"http://{STREAM_SERVER}/dash/{stream_process[sound_id][2]}.mpd",
+                "rtmp-url": f"rtmp://{STREAM_SERVER}:1935/show/{stream_process[sound_id][2]}"
+        })
+
+@app.route("/end-stream", methods=["GET"])
+def end_sound_stream():
+    sound_id = request.args.get("sound-id")
+
+    if sound_id in stream_process:
+        os_kill = subprocess.run(["pkill", "-P", f"{stream_process[sound_id][1].pid}"])
+
+        stream_process.pop(sound_id)
+
+        return json.dumps({
+            "os_kill_code": os_kill.returncode,
+            # "sub_process_kill": sub_process_kill
+        }) 
+        
+    return json.dumps({
+        "error": 500,
+        "message": "IDK what happen too :)))"
+    })
 
 @app.route("/sound-uploader", methods=["GET"])
 def upload_file_template():
--- src/sound.py
+++ src/sound.py
@@ -1,7 +1,8 @@
 import subprocess
 import uuid
 import os
-
+import random
+import string
 import partition
 
 this_dir = os.getcwd()
@@ -16,7 +17,6 @@ def get_wav(parent_dir, full_file_dir: str, width=600, height=120):
         + str(uuid.uuid4())
         + ".png"
     )
-
     bash_cmd = f"""
         ffmpeg -i {full_file_dir} \
             -filter_complex \
@@ -29,8 +29,8 @@ def get_wav(parent_dir, full_file_dir: str, width=600, height=120):
             -frames:v 1 {out_file_name}
     """
     bash_cmd = bash_cmd.strip()
-    print("===================")
-    print(bash_cmd)
+    # print("===================")
+    # print(bash_cmd)
 
     process = subprocess.Popen(
         bash_cmd, bufsize=2048, stdout=subprocess.PIPE, shell=True
@@ -40,3 +40,23 @@ def get_wav(parent_dir, full_file_dir: str, width=600, height=120):
     if process.returncode == 0:
         return partition.move_file(parent_dir, out_file_name)
     return {"error": 500, "message": "IDK what happen too :)))"}
+
+def random_char(y):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(y))
+
+def ffmpeg_stream(parent_dir: str, sound_id: str):
+    sound_file = os.path.join(parent_dir, sound_id)
+    random_str = random_char(10)
+
+    bash_cmd = f"""
+        ffmpeg -re -stream_loop -1 \
+            -i {sound_file} \
+            -vcodec copy -c:a aac -b:a 160k -ar 44100 \
+            -strict -2 -f flv rtmp://128.0.3.2:1935/show/{sound_id.replace("/", random_str)}
+    """
+    bash_cmd = bash_cmd.strip()
+
+    process = subprocess.Popen(
+        bash_cmd, bufsize=2048, stdout=subprocess.PIPE, shell=True
+    )
+    return ( process, random_str )
